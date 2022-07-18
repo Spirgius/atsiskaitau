2247477a39fb7dcc818bb99c302841c8c8d48912
Spirgius <tomasbulotas@gmail.com>
2022-07-18 07:41:45 +0300

    Buvo nespeta :(

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Conversation = () => {
+
+  const currentConvo = useSelector(state => state.chatEngine.value.currentChatRoom)
+
   return (
-    <div className='is-flex-grow-5'>Conversation</div>
+    <div className='is-flex-grow-5'>
+      <button className="button" onClick={()=> console.log(currentConvo)}></button>
+    </div>
   )
 }
 
